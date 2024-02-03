48cf44a0052ba322f9e15ee18640d9db1d5e79a1
BrownCarter <lilbrown3000@gmail.com>
2024-02-03 21:11:41 +0100

    make it better

---
+++ data/data.js
@@ -0,0 +1,85 @@
+export const products = [
+  { id: 1, name: "Hoodie", price: 10, imageUrl: "hoodie.png", sex: "male" },
+  { id: 2, name: "T-Shirt", price: 15, imageUrl: "tee.png", sex: "male" },
+  {
+    id: 7,
+    name: "Pink skirt",
+    price: 45,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/pink-skirt-white_134398-7466.jpg?w=826",
+    sex: "female",
+  },
+  {
+    id: 8,
+    name: "Skirt isolated",
+    price: 32,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/skirt-isolated-white-background_641010-5042.jpg?w=740",
+    sex: "female",
+  },
+  {
+    id: 3,
+    name: "T-Shirt (black)",
+    price: 12,
+    imageUrl:
+      "https://img.freepik.com/free-photo/black-shirt-with-word-ultra-it_1340-37775.jpg?t=st=1706956020~exp=1706959620~hmac=5d8ad3dd285104667615c02b335010caab9ad6a800c94aec7f4085b2a8a02d56&w=740",
+    sex: "male",
+  },
+  {
+    id: 4,
+    name: "T-Shirt (ghost)",
+    price: 16,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/ghost-t-shirt-design-vintage-retro-design_660957-50.jpg?w=360",
+    sex: "male",
+  },
+  {
+    id: 5,
+    name: "T-Shirt (polo)",
+    price: 16,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/polo-tshirt-mockxaup-design_751108-905.jpg?w=740",
+    sex: "male",
+  },
+  {
+    id: 6,
+    name: "Shorts (casual)",
+    price: 6,
+    imageUrl:
+      "https://img.freepik.com/free-photo/casual-men-short-pants_1203-8186.jpg?w=740&t=st=1706956457~exp=1706957057~hmac=1a770fb64f53c4e61eec6ced7946ebcc4827ecea5b074f7b98edd4e9f82c0fea",
+    sex: "male",
+  },
+
+  {
+    id: 9,
+    name: "Short Gown",
+    price: 28,
+    imageUrl:
+      "https://img.freepik.com/free-photo/fashion-woman-with-clothes_1203-8302.jpg?w=360&t=st=1706957108~exp=1706957708~hmac=baecebce9835b3bae686510e3715ad9852956c85c7d300fccd053ecdc27d8569",
+    sex: "female",
+  },
+  {
+    id: 10,
+    name: "Bra 12B",
+    price: 5,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/high-angle-view-bra-against-white-background_1048944-459010.jpg?w=740",
+    sex: "female",
+  },
+  {
+    id: 11,
+    name: "Purple Brassiere",
+    price: 3,
+    imageUrl:
+      "https://img.freepik.com/premium-photo/purple-brassiere-isolated_624181-2175.jpg?w=826",
+    sex: "female",
+  },
+  {
+    id: 12,
+    name: "Wedding Gown",
+    price: 1500,
+    imageUrl:
+      "https://img.freepik.com/free-vector/white-wedding-woman-dress-with-tulle-veil_1441-4087.jpg?w=740&t=st=1706957208~exp=1706957808~hmac=f708a046bc6f11af2dfb1d5cfbcfdaadfa1e08673019f76c1c04f95f6eac7fb8",
+    sex: "female",
+  },
+];
---
+++ index.js
@@ -0,0 +1,108 @@
+import express from "express";
+import "dotenv/config";
+import { affinidiProvider } from "@affinidi/passport-affinidi";
+import { products } from "./data/data.js";
+import nodemailer from "nodemailer";
+
+import cors from "cors";
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const PORT = process.env.PORT || 3003;
+
+const initializeServer = async () => {
+  app.get("/", function (req, res, next) {
+    res.json({ success: "Express" });
+  });
+
+  app.listen(PORT, async () => {
+    /**
+     * Configures Affinidi authentication provider.
+     * @param {Express} app - The Express app instance
+     * @param {Object} options - The configuration options
+     * @param {string} options.id - The provider ID
+     * @param {string} options.issuer - The Affinidi issuer URL
+     * @param {string} options.client_id - The OAuth client ID
+     * @param {string} options.client_secret - The OAuth client secret
+     * @param {string[]} options.redirect_uris - Allowed redirect URIs for OAuth flow
+     */
+    await affinidiProvider(app, {
+      id: "affinidi",
+      issuer: process.env.AFFINIDI_ISSUER,
+      client_id: process.env.AFFINIDI_CLIENT_ID,
+      client_secret: process.env.AFFINIDI_CLIENT_SECRET,
+      redirect_uris: ["http://localhost:3000/auth/callback"],
+    });
+
+    /**
+     * Simple ping route to test if the server is running and responding to requests.
+     * Responds with a JSON message 'Pong'.
+     */
+
+    app.get("/ping", function (req, res) {
+      res.json({ message: "Pong" });
+    });
+
+    /**
+     * Gets all products.
+     * @param {Object} req - Express request object
+     * @param {Object} res - Express response object
+     */
+    app.get("/products", function (req, res) {
+      res.send(products);
+    });
+
+    /**
+     * Gets a single product by ID.
+     * @param {Object} req - Express request object
+     * @param {Object} res - Express response object
+     */
+    app.get("/products/:id", function (req, res) {
+      const { id } = req.params;
+      const product = products.find((p) => p.id == id);
+      if (!product) {
+        res.status(404).send({ message: "Product not found" });
+        return;
+      }
+      res.send(products.find((p) => p.id == id));
+    });
+
+    /**
+     * Sends an email using Mailgun.
+     *
+     * @param {Object} req - Express request object
+     * @param {Object} res - Express response object
+     * @returns {Promise} Sends response with result of sending email
+     */
+
+    app.post("/send", async (req, res) => {
+      const { message, email } = req.body;
+      let transporter = nodemailer.createTransport({
+        host: "smtp.mailgun.org",
+        port: 587,
+        auth: {
+          user: "postmaster@sandbox104beec85e2f4b458b8f4ef87a90c037.mailgun.org",
+          pass: process.env.MAILGUN_PASSWORD,
+        },
+      });
+
+      let info = await transporter.sendMail({
+        from: "postmaster@sandbox104beec85e2f4b458b8f4ef87a90c037.mailgun.org",
+        to: email,
+        subject: "HURRAY 🎈 YOUR PRODUCT IS ON THE WAY",
+        text: message,
+      });
+      res.send({
+        message: "mail has been sent, please check you spam",
+        id: info.messageId,
+      });
+    });
+
+    console.log(`Server listening on ${PORT}`);
+  });
+};
+
+initializeServer();
